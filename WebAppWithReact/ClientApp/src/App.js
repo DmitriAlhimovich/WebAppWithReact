4707e8ac0003c6cc56884fb711420dec0093668e
Александр Магниченко <magniy_s@tut.by>
2021-06-14 17:46:15 +0300

    [*] Сделал стартовой страницей Employees

--- WebAppWithReact/ClientApp/src/App.js
+++ WebAppWithReact/ClientApp/src/App.js
@@ -15,7 +15,8 @@ export default class App extends Component {
   render () {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
+        <Route exact path='/' component={Employees} />
+        <Route exact path='/home' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
         <Route path='/employees' component={Employees} />
