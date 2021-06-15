e648a80248748aa8d1bd0f9d367ef6ad3e059faf
Александр Магниченко <magniy_s@tut.by>
2021-06-15 16:56:13 +0300

    [+] Подключение пользователя

---
+++ WebAppWithReact/Components/TimerViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WebAppWithReact.Components
+{
+    [ViewComponent]
+    public class MyTimer
+    {   public string Invoke()
+        {
+            return $"Текущее время: {DateTime.Now.ToString("hh:mm:ss")}";
+        }
+    }
+}
---
+++ WebAppWithReact/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppWithReact.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } // имя пользователя
+        public int Age { get; set; } // возраст пользователя
+    }
+}
